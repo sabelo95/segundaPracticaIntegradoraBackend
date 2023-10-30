fc97c98646f7198d2cfbed12d9c4b2fe737305ca
Santiago Berrio <santiagoberriolopez@gmail.com>
2023-10-30 18:59:35 -0500

    subiendo tercera entrega desafios backend

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express=require('express')
+
+const PORT=3000
+
+const app=express()
+
+app.get('/products',(req, res)=>{
+    const fs=require('fs')
+    let resultado = JSON.parse(fs.readFileSync('archivos/archivo.json', 'utf-8'));
+     
+   
+    if(req.query.limit){
+        resultado=resultado.slice(0, req.query.limit)
+    }
+
+    res.setHeader('Content-Type','application/json');
+    res.status(200).json({filtros: req.query,resultado });
+})
+
+app.get('/products/:pid',(req,res)=>{
+
+    let id=req.params.pid
+    id=parseInt(id)  
+    if(isNaN(id)){
+        return res.send('Error, ingrese un argumento id numerico')
+    }
+
+    const fs=require('fs')
+    let resultado = JSON.parse(fs.readFileSync('archivos/archivo.json', 'utf-8')).find(prod=>prod.id===id);
+    res.setHeader('Content-Type','application/json');
+    res.status(200).json({resultado});
+})
+
+
+const server=app.listen(PORT, ()=>{
+    console.log(`Server on line en puerto ${PORT}`)
+})
---
+++ src/entrega2.js
@@ -0,0 +1,137 @@
+class ProductManager {
+    constructor(path) {
+        this.products = [];
+        this.path=path
+    }
+
+    addProduct(title, description, price, thumbnail, code, stock) {
+       
+        const missingFields = [];
+    
+        
+        if (!title) {
+            missingFields.push("Título");
+        }
+        if (!description) {
+            missingFields.push("Descripción");
+        }
+        if (!price) {
+            missingFields.push("Precio");
+        }
+        if (!thumbnail) {
+            missingFields.push("Thumbnail");
+        }
+        if (!code) {
+            missingFields.push("Código");
+        }
+        if (stock === undefined) {
+            missingFields.push("Stock");
+        }
+    
+        if (missingFields.length > 0) {
+            console.log(`Los siguientes campos son obligatorios y faltan: ${missingFields.join(", ")}`);
+        } else if (this.products.find((prod) => prod.code === code)) {
+            console.log(`El producto con código ${code} ya existe`);
+        } else {
+            let id = this.products.length;
+            this.products.push({ title, description, price, thumbnail, code, stock, id });
+        }
+
+        const fs=require('fs')
+        fs.writeFileSync(this.path, JSON.stringify(this.products,null,'\t'))
+    }
+    
+
+    getProducts(){
+        const fs=require('fs')
+        console.log(JSON.parse(fs.readFileSync(this.path, "utf-8")))
+        
+    }
+
+    getProdcutsById(id){
+        const fs=require('fs')
+        const lectura = fs.readFileSync(this.path, 'utf-8');
+        const arrayJSON = JSON.parse(lectura);
+        arrayJSON.find((prod) => prod.id === id) ?
+        console.log(this.products.find((prod) => prod.id === id)) :
+        console.log('Not found');
+
+       
+        
+    }
+
+   /*  updateProduct(title, description, price, thumbnail, code, stock, id) {
+        const fs = require('fs');
+        const lectura = fs.readFileSync(this.path, 'utf-8');
+        const arrayJSON = JSON.parse(lectura);
+    
+        const updatedArray = arrayJSON.map((prod) => {
+            if (prod.id === id) {
+                return {
+                    ...prod,
+                    title: title,
+                    description: description,
+                    price: price,
+                    thumbnail: thumbnail,
+                    code: code,
+                    stock: stock,
+                };
+            } else {
+                return prod;
+            }
+        });
+    
+        fs.writeFileSync(this.path, JSON.stringify(updatedArray, null, '\t'));
+    
+        return updatedArray;
+    }
+ */
+
+    updateProduct(id, updates) {
+        const fs = require('fs');
+        const lectura = fs.readFileSync(this.path, 'utf-8');
+        const arrayJSON = JSON.parse(lectura);
+    
+        const updatedArray = arrayJSON.map((prod) => {
+            if (prod.id === id) {
+                return {
+                    ...prod,
+                    ...updates,
+                };
+            } else {
+                return prod;
+            }
+        });
+    
+        fs.writeFileSync(this.path, JSON.stringify(updatedArray, null, '\t'));
+    
+        return updatedArray;
+    }
+    
+    deleteProducto(id){
+        const fs = require('fs');
+        const lectura = fs.readFileSync(this.path, 'utf-8');
+        const arrayJSON = JSON.parse(lectura);
+        const updatedArray = arrayJSON.filter(prod => prod.id !== id); 
+            
+
+        
+        fs.writeFileSync(this.path, JSON.stringify(updatedArray, null, '\t'));
+    
+        return updatedArray;
+            
+                
+    }
+}    
+
+const manager = new ProductManager('archivos/archivo.json');
+manager.addProduct('play','play station', 2000,'lalalal',3,30);
+manager.addProduct('play','play station', 2000,'lalalal',2,30);
+manager.updateProduct('xbox','xbox', 2000,'lalalal',2,30,1);
+manager.updateProduct(0,{title: 'nintento', code: 50});
+
+
+
+
+
+
